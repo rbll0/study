33d25a3958a1f12f8fdefee7527f4f59d6389aca
Gustavo Rabelo Frere <gurabelo09@gmail.com>
2025-11-11 20:06:50 -0300

    feat: add example of list manipulation in Python

---
+++ python/projetos/modulo-1/tipos-variaveis/list.py
@@ -0,0 +1,12 @@
+# Declaração 
+
+# Exibindo a lista
+my_list = [1,2,3,4,5, "Python", True, False]
+print("Minha lista:", my_list)
+
+# Exibindo os elementos da lista
+print("my_list[0]",my_list[0])  # Primeiro elemento
+print("my_list[5]",my_list[5])  # Quinto elemento
+print("my_list[2:6]:",my_list[2:6]) # Fatiamento da lista do índice 2 ao 5
+print("my_list[:6]:",my_list[:6]) # Fatiamento da lista do início ao índice 5
+print("my_list[2:]:",my_list[2:]) # Fatiamento da lista do índice 2 até o final
