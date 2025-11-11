589bd66bb4dda67ce144f442b009b97a46fb8df9
Gustavo Rabelo Frere <gurabelo09@gmail.com>
2025-11-11 20:44:33 -0300

    feat: add example of tuples in Python

---
+++ python/projetos/modulo-1/tipos-variaveis/tuplas.py
@@ -0,0 +1,18 @@
+# Coleção ordenada e imutável
+
+# Exemplo de tupla
+my_tuple = (1, 2, 2, 3, 4)
+
+print("Tupla:", my_tuple)
+
+# Acessando elementos
+print("Primeiro elemento:", my_tuple[0])
+print("Último elemento:", my_tuple[-1])
+
+# Método count
+count = my_tuple.count(1)
+print("O número 1 aparece", count, "vezes na tupla.")
+
+# Método index
+index = my_tuple.index(2)
+print("O primeiro índice do número 2 é:", index)
