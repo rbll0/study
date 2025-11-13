0bd11c5c9c8a6c3d61f5a2147d1f3f121e706ee7
Gustavo Rabelo Frere <gurabelo09@gmail.com>
2025-11-13 09:02:29 -0300

    feat: add functions to complete and remove completed tasks

--- python/aula/projeto/gerenciador.py
+++ python/aula/projeto/gerenciador.py
@@ -23,7 +23,22 @@ def atualizar_tarefa(tarefas, indice, novo_nome):
         print(f"Tarefa {indice} atualizada para {novo_nome}" )
     else:
         print("Índice inválido.")
-            
+    return
+
+def completar_tarefa(tarefas,indice):
+    indice_ajustado = int(indice) - 1
+    if indice_ajustado >= 0 and indice_ajustado < len(tarefas):
+        tarefas[indice_ajustado]["completa"] = True
+    else:
+        print("Índice inválido.")
+    print("Tarefa marcada como completa.")
+
+def remover_tarefa_completa(tarefas):
+    print("\nRemovendo tarefas completas...")
+    for tarefa in tarefas:
+        if tarefa["completa"] == True:
+            tarefas.remove(tarefa)
+            print(f'Tarefa "{tarefa["tarefa"]}" removida com sucesso!')
     return
 
 while True:
@@ -47,9 +62,18 @@ while True:
 
     elif opcao == '3':
         ver_tarefas(tarefas)
-        indice = int(input("Digite o número da tarefa que deseja atualizar: "))
+        indice = input("Digite o número da tarefa que deseja atualizar: ")
         novo_nome = input("Digite o novo nome da tarefa: ")
         atualizar_tarefa(tarefas, indice, novo_nome)
+
+    elif opcao == '4':   
+        ver_tarefas(tarefas)
+        indice = input("Digite o número da tarefa que deseja marcar como completa: ")
+        completar_tarefa(tarefas, indice)
+
+    elif opcao == '5':
+        remover_tarefa_completa(tarefas)
+        ver_tarefas(tarefas)
     elif opcao == '6':
         print("Saindo do gerenciador de tarefas.")
         break
