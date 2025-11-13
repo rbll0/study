e6c1c0679161c9f2116eb09510e6d4573c9c813e
Gustavo Rabelo Frere <gurabelo09@gmail.com>
2025-11-12 21:59:18 -0300

    feat: implement function to add tasks in the task manager

--- python/aula/projeto/gerenciador.py
+++ python/aula/projeto/gerenciador.py
@@ -1,3 +1,13 @@
+tarefas = []
+
+def adicionar_tarefa(tarefas, nome_tarefa):
+    tarefas.append({
+        "Tarefa": nome_tarefa,
+        "completa": False
+    })
+    print(f'Tarefa "{nome_tarefa}" adicionada com sucesso!')
+    return
+
 while True:
     print("\nMenu do Gerenciador de Tarefas")
     print("1. Adicionar Tarefa")
@@ -9,6 +19,10 @@ while True:
 
     opcao = input("Escolha uma opção (1-6): ")
 
-    if opcao == '6':
+    if opcao == '1': 
+        nome_tarefa = input("Digite o nome da tarefa: ")
+        adicionar_tarefa(tarefas, nome_tarefa)
+
+    elif opcao == '6':
         print("Saindo do gerenciador de tarefas.")
         break
